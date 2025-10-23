8027e7aa7a40ba837bd82e890d7c4c926f57ce7d
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2025-10-23 16:23:15 +0530

    Time: 0 ms (100%), Space: 63.6 MB (47.78%) - LeetHub

--- 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.cpp
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.cpp
@@ -5,14 +5,15 @@ public:
 
         while(low <= high) {
             mid = high +  (low - high) / 2;
-            if(arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) {
+
+            if(arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) {    // if mid is the peak
                 return mid;
-            } else if(arr[mid] > arr[mid - 1]) {
+            } else if(arr[mid] > arr[mid - 1]) {      // if peak ele. in right side of mid ele.
                 low = mid + 1;
             } else {
-                high = mid - 1;
+                high = mid - 1;     // if peak ele. in the left side of mid ele.
             }
         }
-        return -1;
+        return -1;  // array is null
     }
 };

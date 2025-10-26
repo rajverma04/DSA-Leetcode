f83029aadca7ef63d8f9ea1e368dcd7a407b1fd9
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2025-10-26 22:07:48 +0530

    Time: 0 ms (100%), Space: 8.6 MB (71.24%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -7,13 +7,13 @@ public:
         if(dividend == INT_MIN && divisor == -1) {
             return INT_MAX;
         }
-        long long result = (long long)dividend / (long long)divisor;
+        // long long result = (long long)dividend / (long long)divisor;
         // if(dividend > INT_MAX - 1) {
         //     return INT_MAX;
         // }
         // if(dividend < INT_MAX) {
         //     return INT_MIN;
         // }
-        return (int)result;
+        return dividend / divisor;
     }
 };

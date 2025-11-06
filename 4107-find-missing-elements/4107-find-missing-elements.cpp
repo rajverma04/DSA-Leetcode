1bac3615767bcde8fb020b3804f9cc8633256213
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2025-11-06 21:00:45 +0530

    Time: 3 ms (59.5%), Space: 32.3 MB (79.42%) - LeetHub

--- 4107-find-missing-elements/4107-find-missing-elements.cpp
+++ 4107-find-missing-elements/4107-find-missing-elements.cpp
@@ -9,26 +9,26 @@ public:
         }
 
         vector<int> ans;
-        // for(int i = mn; i <= mx; i++) {
-        //     bool isfound = false;
-        //     for(int j = 0; j < nums.size(); j++) {
-        //         if(i == nums[j]) {
-        //             isfound = true;
-        //         }
-        //     }
-        //     if(!isfound) {
-        //         ans.push_back(i);
-        //     }
-        // }
-
-        // M2 --> using unordered_set: Elements are unique(not in key:value pair present)
-        unordered_set<int> arr(nums.begin(), nums.end());   // copying nums element into arr
-
         for(int i = mn; i <= mx; i++) {
-            if(arr.find(i) == arr.end()) {      // if not found in arr
+            bool isfound = false;
+            for(int j = 0; j < nums.size(); j++) {
+                if(i == nums[j]) {
+                    isfound = true;
+                }
+            }
+            if(!isfound) {
                 ans.push_back(i);
             }
         }
+
+        // M2 --> using unordered_set: Elements are unique(not in key:value pair present)
+        // unordered_set<int> arr(nums.begin(), nums.end());   // copying nums element into arr
+
+        // for(int i = mn; i <= mx; i++) {
+        //     if(arr.find(i) == arr.end()) {      // if not found in arr
+        //         ans.push_back(i);
+        //     }
+        // }
 
         return ans;
     }

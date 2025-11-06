774c31dbe28e62128b9c0c2025313b359826ff68
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2025-11-06 21:58:19 +0530

    Time: 54 ms (39.58%), Space: 90.8 MB (44.94%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,20 +1,32 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int, int> freq;
-
+        // M1 -->  unordered_set(contain distinct element)
+        unordered_set<int> seen;
         for(auto x : nums) {
-            freq[x]++;
-        }
-
-        bool duplicatePresent = false;
-        for(auto x : freq) {
-            if(x.second != 1) {
-                duplicatePresent = true;
-                break;
+            if(seen.count(x)) {
+                return true;        // duplicate present
             }
+            seen.insert(x);
         }
 
-        return duplicatePresent;
+        return false;       // duplicate not found
+
+        // M2 --> freq method
+        // unordered_map<int, int> freq;
+
+        // for(auto x : nums) {
+        //     freq[x]++;
+        // }
+
+        // bool duplicatePresent = false;
+        // for(auto x : freq) {
+        //     if(x.second != 1) {
+        //         duplicatePresent = true;
+        //         break;
+        //     }
+        // }
+
+        // return duplicatePresent;
     }
 };

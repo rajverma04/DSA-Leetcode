c5d91699dcbbbe5b2e6e06898bba948ccf8e3dde
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2025-11-13 00:53:47 +0530

    Time: 48 ms (79.62%), Space: 56.4 MB (63.74%) - LeetHub

---
+++ 2789-counter-ii/2789-counter-ii.js
@@ -0,0 +1,25 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let val = init;
+    return {
+        increment: () => {
+            return ++init;
+        },
+        decrement: () => {
+            return --init;
+        },
+        reset: () => {
+            return init = val;
+        }
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */

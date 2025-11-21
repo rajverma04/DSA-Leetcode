9802c036ebc8e0c204aad30524ad091c7b772a1c
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2025-11-21 19:47:09 +0530

    Time: 7 ms (5.1%), Space: 10.7 MB (5.28%) - LeetHub

---
+++ 1048-clumsy-factorial/1048-clumsy-factorial.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int clumsy(int n) {
+        stack<int> st1;
+        int num;
+        st1.push(n--);
+
+        int i = 0;  // 0: *, 1: /, 2: +, 3: -
+        while(n) {
+            if(i == 0) {    // multiply
+                num = st1.top();
+                st1.pop();
+                st1.push(num * n);
+            } else if(i == 1) {     // divide
+                num = st1.top();
+                st1.pop();
+                st1.push(num / n);
+            } else {
+                st1.push(n);
+            }
+            i = (i + 1) % 4;        // sign change
+            n--;
+        }
+
+        stack<int> st2;
+        while(st1.size()) {
+            st2.push(st1.top());
+            st1.pop();
+        }
+
+        int sum = st2.top();
+        st2.pop();
+
+        bool flag = false;
+        while(st2.size()) {
+            if(!flag) {             // addition
+                sum += st2.top();
+            } else {        // substraction
+                sum -= st2.top();
+            }
+            st2.pop();
+            flag = !flag;
+        }
+
+        return sum;
+    }
+};

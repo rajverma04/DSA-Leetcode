6d7cf55fd8dc0f683cc7fde20be807d4d3c3e5ee
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2025-12-11 11:29:03 +0530

    Time: 8 ms (6.66%), Space: 8.8 MB (68.41%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.cpp
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        int stair = 0;
 
         int count = 1;
         while(n >= count) {
-            stair++;
             n -= count;
             count++;
         }

392b676bf93be644f2fae77782f5d8aa6016f482
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2025-12-22 01:23:15 +0530

    Time: 55 ms (68.75%), Space: 70.8 MB (98.84%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    void heapify(vector<int> &nums, int size, int index) {
+        int largest = index;
+        int left = 2 * index + 1;
+        int right = 2 * index + 2;
+
+        if(left < size && nums[left] > nums[largest]) {
+            largest = left;
+        }
+
+        if(right < size && nums[right] > nums[largest]) {
+            largest = right;
+        }
+
+        if(largest != index) {
+            swap(nums[largest], nums[index]);
+            heapify(nums, size, largest);
+        }
+    }
+
+    vector<int> sortArray(vector<int>& nums) {
+        for(int i = nums.size() / 2 - 1; i >= 0; i--) {
+            heapify(nums, nums.size(), i);
+        }
+
+        // sort
+        for(int i = nums.size() - 1; i >= 0; i--) {
+            swap(nums[i], nums[0]);
+            heapify(nums, i, 0);
+        }
+
+        return nums;
+    }
+};

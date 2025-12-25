3535fdd9ea9b9d978f3ad661a4a28b557becbd18
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2025-12-25 21:50:58 +0530

    Time: 3 ms (67.73%), Space: 14.8 MB (37.19%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,19 +1,39 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& arr, int target) {
-        int left = 0, right = 1;
-        int n = arr.size();
+        // M1: Two pointer
+        // int left = 0, right = 1;
+        // int n = arr.size();
         
-        while (left < n) {
-            right = left + 1;
-            while (right < n) {
-                if (arr[left] + arr[right] == target) {
-                    return {left, right};
+        // while (left < n) {
+        //     right = left + 1;
+        //     while (right < n) {
+        //         if (arr[left] + arr[right] == target) {
+        //             return {left, right};
+        //         }
+        //         right++;
+        //     }
+        //     left++;
+        // }
+        // return {};
+
+
+        // M2: Using Hashing(unordered_map)
+
+        unordered_map<int, int> mp;
+        vector<int> ans;
+        for(int i = 0; i < arr.size(); i++) {
+            if(mp.count(target - arr[i])) {
+                ans.push_back(mp[target - arr[i]]);
+                ans.push_back(i);
+                return ans;
+            } else {
+                if(mp.count(arr[i]) == 0) {
+                    mp[arr[i]] = i;
                 }
-                right++;
             }
-            left++;
         }
-        return {};
+
+        return ans;
     }
 };

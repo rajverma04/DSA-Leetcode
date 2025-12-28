9fe1ad025669c721ed284f6358aae75711f79cb8
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2025-12-28 19:13:24 +0530

    Time: 2 ms (84.8%), Space: 47.2 MB (40.33%) - LeetHub

--- 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.cpp
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.cpp
@@ -11,22 +11,43 @@
 class Solution {
 public:
     vector<int> nextLargerNodes(ListNode* head) {
-        vector<int> ans;
 
-        ListNode *curr = head, *temp = head;
+        // M1: brute force(O(n*n))
+        // vector<int> ans;
 
-        while(curr) {
-            int nextGreater = 0;
-            while(temp) {
-                if(temp->val > curr->val) {
-                    nextGreater = temp->val;
-                    break;
-                }
-                temp = temp->next;
+        // ListNode *curr = head, *temp = head;
+
+        // while(curr) {
+        //     int nextGreater = 0;
+        //     while(temp) {
+        //         if(temp->val > curr->val) {
+        //             nextGreater = temp->val;
+        //             break;
+        //         }
+        //         temp = temp->next;
+        //     }
+        //     ans.push_back(nextGreater);
+        //     temp = curr->next;
+        //     curr = curr->next;
+        // }
+        // return ans;
+
+
+        // M2: stack(O(n))
+        vector<int> values;
+        for(ListNode *curr = head; curr; curr = curr->next) {
+            values.push_back(curr->val);
+        }
+        int n = values.size();
+        vector<int> ans(n, 0);      // store 0 initially for those whose nextGreater ele. does not exist
+        stack<int> st;      // store the index of whose nextGreater element is not found
+
+        for(int i = 0; i < n; i++) {
+            while(!st.empty() && values[i] > values[st.top()]) {
+                ans[st.top()] = values[i];
+                st.pop();
             }
-            ans.push_back(nextGreater);
-            temp = curr->next;
-            curr = curr->next;
+            st.push(i);
         }
 
         return ans;

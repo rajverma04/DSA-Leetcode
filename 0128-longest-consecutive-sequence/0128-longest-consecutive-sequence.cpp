44ff43efd03f23c9401456aa2be14a9553034121
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2026-01-06 19:35:48 +0530

    Time: 77 ms (73.93%), Space: 89 MB (41.68%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -4,22 +4,42 @@ public:
         if(nums.size() == 0) {
             return 0;
         }
-        sort(nums.begin(), nums.end());
+        // sort(nums.begin(), nums.end());
 
         int maxi = 0;
-        int count = 0;
-        for(int i = 0; i < nums.size() - 1; i++) {
-            if((nums[i+1] - nums[i]) == 1) {
-                count++;
-            }
+        // int count = 0;
+        // for(int i = 0; i < nums.size() - 1; i++) {
+        //     if((nums[i+1] - nums[i]) == 1) {
+        //         count++;
+        //     }
 
-            if((nums[i+1] - nums[i]) > 1) {
-                count = 0;
-            }
-            maxi = max(maxi, count);
+        //     if((nums[i+1] - nums[i]) > 1) {
+        //         count = 0;
+        //     }
+        //     maxi = max(maxi, count);
 
+        // }
+
+        // return maxi+1;
+
+
+        // M2: unordered_set
+        unordered_set<int> st(nums.begin(), nums.end());
+
+        for(auto num : st) {
+            if(st.find(num - 1) == st.end()) {
+                int currentNum = num;
+                int count = 1;
+
+                while(st.find(currentNum + 1) != st.end()) {
+                    currentNum++;
+                    count++;
+                }
+                maxi = max(maxi, count);
+
+            }
         }
 
-        return maxi+1;
+        return maxi;
     }
 };

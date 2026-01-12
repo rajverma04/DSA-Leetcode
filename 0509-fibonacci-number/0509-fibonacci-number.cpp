196030b71f636967a159f225ab96ea49b19309b1
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2026-01-13 02:05:22 +0530

    Time: 0 ms (100%), Space: 7.8 MB (36.07%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -4,13 +4,15 @@ public:
         if(n <= 1) {
             return n;
         }
-        vector<int> dp(n + 1);
-        dp[0] = 0;
-        dp[1] = 1;
+        int first = 0;
+        int second = 1;
+        int third;
 
         for(int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+            third = first + second;
+            first = second;
+            second = third;
         }
-        return dp[n];
+        return third;
     }
 };

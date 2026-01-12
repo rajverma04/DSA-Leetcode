ae276dd85a336c2d0c8dcee2a7ee7346c32fff58
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2026-01-13 02:27:42 +0530

    Time: 0 ms (100%), Space: 8.5 MB (33.71%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-        if (n <= 2) {
-            return n;       
-        } 
-
-        int a = 1, b = 2;          // f(1), f(2)
-        for (int i = 3; i <= n; ++i) {
-            int c = a + b;         // f(i) = f(i-1) + f(i-2)
-            a = b;
-            b = c;
+    int step(int stair, int n, vector<int> &dp) {
+        if(stair == n) {
+            return 1;
         }
-        return b;
+        if(stair > n) {
+            return 0;
+        }
 
-        // Using recursion time complexity increases
-        // if(n == 1) {
-        //     return 1;
-        // }
-        // if(n == 2) {
-        //     return 2;
-        // }
-        // return climbStairs(n - 1) + climbStairs(n - 2);
+        if(dp[stair] != -1) {
+            return dp[stair];
+        }
+
+        dp[stair] = step(stair + 1, n, dp) + step(stair + 2, n, dp);
+        return dp[stair];
+    }
+    int climbStairs(int n) {
+        vector<int> dp(n + 2, -1);
+
+        return step(0, n, dp);
     }
 };

84a5795cc1dc82802a255d531a47a0bf7011957f
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2026-01-20 01:02:08 +0530

    Time: 0 ms (100%), Space: 25.6 MB (49.33%) - LeetHub

--- 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.cpp
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.cpp
@@ -11,33 +11,53 @@
  */
 class Solution {
 public:
+
+    int inOrder(TreeNode *root, TreeNode* &prev, int &ans) {
+        if(!root) {
+            return 0;
+        }
+        inOrder(root->left, prev, ans);
+        if(prev) {
+            ans = min(ans, root->val - prev->val);
+        }
+        prev = root;
+        inOrder(root->right, prev, ans);
+        
+        return ans;
+    }
     int getMinimumDifference(TreeNode* root) {
         if(!root) {
             return 0;
         }
-        vector<int> arr;
-        queue<TreeNode*> q;
-        q.push(root);
+        // vector<int> arr;
+        // queue<TreeNode*> q;
+        // q.push(root);
 
-        while(!q.empty()) {
-            auto node = q.front();
-            q.pop();
+        // while(!q.empty()) {
+        //     auto node = q.front();
+        //     q.pop();
 
-            arr.push_back(node->val);
-            if(node->left) {
-                q.push(node->left);
-            }
-            if(node->right) {
-                q.push(node->right);
-            }
-        }
+        //     arr.push_back(node->val);
+        //     if(node->left) {
+        //         q.push(node->left);
+        //     }
+        //     if(node->right) {
+        //         q.push(node->right);
+        //     }
+        // }
 
-        sort(arr.begin(), arr.end());
+        // sort(arr.begin(), arr.end());
+        // int ans = INT_MAX;
+        // for(int i = 1; i < arr.size(); i++) {
+        //     ans = min(ans, arr[i] - arr[i - 1]);
+        // }
+
+        // return ans;
+
+        // Inorder traversal: element in sorted
         int ans = INT_MAX;
-        for(int i = 1; i < arr.size(); i++) {
-            ans = min(ans, arr[i] - arr[i - 1]);
-        }
+        TreeNode *prev = nullptr;
 
-        return ans;
+        return inOrder(root, prev, ans);;
     }
 };

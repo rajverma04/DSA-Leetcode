b56d9b857c2d60cef3745c79bf8eb51060752bb1
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2026-01-21 18:55:27 +0530

    Time: 0 ms (100%), Space: 32.1 MB (72.4%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-        unordered_map<int, int> mp;
-        mp[0] = 1;
         int minSize = INT_MAX;
         int start = 0, end = 0, n = nums.size();
         int prefixSum = 0;

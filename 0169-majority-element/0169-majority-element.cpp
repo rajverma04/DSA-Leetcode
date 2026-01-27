f4c1823e7cbecadc4da9a6ecbfc7b0647a703933
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2026-01-28 01:33:34 +0530

    Time: 0 ms (100%), Space: 28.2 MB (62.48%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,20 +1,34 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int, int> freq;
+        // unordered_map<int, int> freq;
 
-        for(auto x : nums) {
-            freq[x]++;
-        }
+        // for(auto x : nums) {
+        //     freq[x]++;
+        // }
 
-        int majorityElement = -1;
-        for(auto x : freq) {
-            if(x.second > nums.size() / 2) {
-                majorityElement = x.first;
-                break;
+        // int majorityElement = -1;
+        // for(auto x : freq) {
+        //     if(x.second > nums.size() / 2) {
+        //         majorityElement = x.first;
+        //         break;
+        //     }
+        // }
+
+        // M2 : Moore's Voting algorithm
+    
+        int count = 0, element = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            if(count == 0) {
+                element = nums[i];
+            }
+            if(nums[i] == element) {
+                count++;
+            } else {
+                count--;
             }
         }
 
-        return majorityElement;
+        return element;
     }
 };

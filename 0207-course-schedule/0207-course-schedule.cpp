2cfc5ecb608bd847b0d143418c9c504ba30bd7d2
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2026-01-31 17:27:53 +0530

    Time: 11 ms (23.68%), Space: 20.3 MB (20.05%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        for(auto e : prerequisites) {
+            int u = e[0];
+            int v = e[1];
+            adj[v].push_back(u);
+        }
+
+        vector<int> inDeg(numCourses, 0);
+        for(int i = 0; i < numCourses; i++) {
+            for(auto it : adj[i]) {
+                inDeg[it]++;
+            }
+        }
+
+        queue<int> q;
+        vector<int> ans;
+
+        for(int i = 0; i < numCourses; i++) {
+            if(inDeg[i] == 0) {
+                q.push(i);
+            }
+        }
+
+        while(!q.empty()) {
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+            
+            for(int i = 0; i < adj[node].size(); i++) {
+                inDeg[adj[node][i]]--;
+                if(inDeg[adj[node][i]] == 0) {
+                    q.push(adj[node][i]);
+                }
+            }
+        }
+        if(ans.size() == numCourses) {
+            return true;
+        }
+        return false;
+    }
+};

86f3aec1e884186a27a0a54f668875116f0a45a0
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2026-02-02 02:29:26 +0530

    Time: 0 ms (100%), Space: 30.3 MB (100%) - LeetHub

--- 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.cpp
+++ 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.cpp
@@ -3,7 +3,15 @@ public:
     int minimumCost(vector<int>& nums) {
         // first element is always included need to find two minimum elment after 0 index
         
-        sort(nums.begin() + 1, nums.end());
-        return nums[0] + nums[1] + nums[2];
+        // sort(nums.begin() + 1, nums.end());
+        // return nums[0] + nums[1] + nums[2];
+        int ans = INT_MAX;
+
+        for(int i=1;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
+                ans = min(ans, nums[i] + nums[j]);
+            }
+        }
+        return  nums[0] + ans;
     }
 };

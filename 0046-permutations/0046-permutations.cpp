a0b70385d9fce8184e87e249d7a5d150a6ebfe48
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2026-02-02 22:23:25 +0530

    Time: 0 ms (100%), Space: 10.5 MB (70.43%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -3,6 +3,7 @@ public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
         sort(nums.begin(), nums.end());
+        // next permutation works efficiently when array is sorted
         do {
             ans.push_back(nums);
         } while(next_permutation(nums.begin(), nums.end()));

6f157700e8ef2935efde1e63e134e1f56e609e8d
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2026-02-03 10:04:04 +0530

    Time: 0 ms (100%), Space: 27.8 MB (35.38%) - LeetHub

--- 3637-trionic-array-i/3637-trionic-array-i.cpp
+++ 3637-trionic-array-i/3637-trionic-array-i.cpp
@@ -4,38 +4,25 @@ public:
         int n = nums.size();
         if (n < 3) return false;
 
-        int phase = 0; // 0: inc, 1: dec, 2: inc
-        bool inc1 = false, dec = false, inc2 = false;
+        int i = 1;
 
-        for (int i = 1; i < n; i++) {
-            if (nums[i] == nums[i - 1])
-                return false;
+        // strictly increasing
+        while (i < n && nums[i] > nums[i - 1]) {
+            i++;
+        }
+        if (i == 1 || i == n) return false;  // no increasing part
 
-            if (phase == 0) {
-                if (nums[i] > nums[i - 1]) {
-                    inc1 = true;
-                } else {
-                    phase = 1;
-                    dec = true;
-                }
-            }
-            else if (phase == 1) {
-                if (nums[i] < nums[i - 1]) {
-                    dec = true;
-                } else {
-                    phase = 2;
-                    inc2 = true;
-                }
-            }
-            else { // phase == 2
-                if (nums[i] > nums[i - 1]) {
-                    inc2 = true;
-                } else {
-                    return false;
-                }
-            }
+        //  strictly decreasing
+        while (i < n && nums[i] < nums[i - 1]) {
+            i++;
+        }
+        if (i == n) return false;  // no decreasing part
+
+        //  strictly increasing
+        while (i < n && nums[i] > nums[i - 1]) {
+            i++;
         }
 
-        return inc1 && dec && inc2;
+        return i == n;
     }
 };

d64d4d214094c92ae6facdd78d92a5c45eb0ff39
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2026-02-05 19:02:29 +0530

    Time: 0 ms (100%), Space: 29.6 MB (43.72%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -2,24 +2,34 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
-        deque<int> dq;
 
-        for(auto x : nums) {
-            dq.push_back(x);
-        }
+        // O(n + k)
+        // deque<int> dq;
 
-        while(k != 0) {
-            int last = dq.back();
-            dq.pop_back();
-            dq.push_front(last);
-            k--;
-        }
+        // for(auto x : nums) {
+        //     dq.push_back(x);
+        // }
 
-        int i = 0;
-        while(!dq.empty()) {
-            nums[i] = dq.front();
-            dq.pop_front();
-            i++;
-        }
+        // while(k != 0) {
+        //     int last = dq.back();
+        //     dq.pop_back();
+        //     dq.push_front(last);
+        //     k--;
+        // }
+
+        // int i = 0;
+        // while(!dq.empty()) {
+        //     nums[i] = dq.front();
+        //     dq.pop_front();
+        //     i++;
+        // }
+
+        // M2 : using reverse(which itself uses two pointer)
+        
+        k %= n;     // base case: because when we do reverse it will go out of boundary
+
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
     }
 };

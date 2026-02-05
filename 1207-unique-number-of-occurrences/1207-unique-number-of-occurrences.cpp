48e7637f5dc856fa2a773fc067d05e28659e6109
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2026-02-05 13:41:41 +0530

    Time: 0 ms (100%), Space: 11.9 MB (78.91%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -5,14 +5,22 @@ public:
         for(auto x : arr) {
             mp[x]++;
         }
+        // for(auto it : mp) {      // O(n*n)
+        //     for(auto x : mp) {
+        //         if(it.first == x.first) {
+        //             continue;
+        //         } else if(it.second == x.second) {
+        //             return false;
+        //         }
+        //     }
+        // }
+
+        unordered_set<int> st;
         for(auto it : mp) {
-            for(auto x : mp) {
-                if(it.first == x.first) {
-                    continue;
-                } else if(it.second == x.second) {
-                    return false;
-                }
+            if(st.count(it.second)) {
+                return false;
             }
+            st.insert(it.second);
         }
         
         return true;

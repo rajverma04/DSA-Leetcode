5d43ebe544058ca5d35c99eb18c8d2b4e0046ac9
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2026-02-06 21:23:38 +0530

    Time: 65 ms (9.27%), Space: 31.6 MB (16.1%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,30 @@
+// eg3: leetcode
+// step1 : find the longest palindrome in given string(leet)
+// step2 : leetcodocteel (n - longest palindrome subsequence)
+
+class Solution {
+public:
+    int find(int i, int j, string &s1, string &s2, vector<vector<int>> &dp) {
+        if(i == 0 || j == 0) {
+            return 0;
+        }
+
+        if(dp[i][j] != -1) {
+            return dp[i][j];
+        }
+
+        if(s1[i - 1] == s2[j - 1]) {
+            return dp[i][j] = 1 + find(i - 1, j - 1, s1, s2, dp);
+        }
+
+        return dp[i][j] = max(find(i - 1, j, s1, s2, dp), find(i, j - 1, s1, s2, dp));
+    }
+    int minInsertions(string s) {
+        int n = s.size();
+        string s2 = s;
+        reverse(s2.begin(), s2.end());
+        vector<vector<int>> dp(n + 1, vector<int> (n + 1, -1));
+        return n - find(n, n, s, s2, dp);
+        
+    }
+};

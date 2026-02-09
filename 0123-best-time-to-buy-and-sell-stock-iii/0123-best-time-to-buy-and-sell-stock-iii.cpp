43fead951fc53d24d867b4120d01fef0c2b8b669
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2026-02-10 01:24:52 +0530

    Time: 459 ms (19.98%), Space: 225.5 MB (13.28%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -7,21 +7,23 @@ public:
         if(dp[idx][buy][cap] != -1) {
             return dp[idx][buy][cap];
         }
-
+        
+        // jb hum stock lete hai to purse price km hoga isliye minus hai
         if(buy) {       // buy
-            return dp[idx][buy][cap] = max(-prices[idx] + find(idx + 1, n, 0, cap, prices, dp), 
-                                            0 + find(idx + 1, n, 1, cap, prices, dp)
+            return dp[idx][buy][cap] = max(-prices[idx] + find(idx + 1, n, 0, cap, prices, dp),         // buy stock here & cap remains same
+                                            0 + find(idx + 1, n, 1, cap, prices, dp)        // not buyed stock here & cap remains same
                                             );
         } else {        // sell
-            return dp[idx][buy][cap] = max(prices[idx] + find(idx + 1, n, 1,  cap - 1, prices, dp), 
-                                            0 + find(idx + 1, n, 0, cap, prices, dp)
+            // jb hum stock profit me sell karte hai to purse price badhega 
+            return dp[idx][buy][cap] = max(prices[idx] + find(idx + 1, n, 1,  cap - 1, prices, dp),         // sold stock here & cap reduced as sold
+                                            0 + find(idx + 1, n, 0, cap, prices, dp)                    // not sold stock here so cap remains same
                                             );
         }
 
     }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(3, -1)));
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(3, -1)));      // 3 cap{2: 2 full buy+sell transactions, 1 : one transaction still can do, 0:  no transaction}
 
         return find(0, n, 1, 2, prices, dp);        // cap : 2 transactions allowed
     }

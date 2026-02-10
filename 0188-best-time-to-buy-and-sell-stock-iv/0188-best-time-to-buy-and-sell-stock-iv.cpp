7b64f999662a8d4cc8a03cfb1f524c96dc3ed24a
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2026-02-10 23:31:46 +0530

    Time: 15 ms (30.83%), Space: 18.3 MB (39.2%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int find(int idx, int n, int buy, int cap, vector<int>& prices, vector<vector<vector<int>>> &dp) {
+        if(idx == n || cap == 0) {
+            return 0;
+        }
+        if(dp[idx][buy][cap] != -1) {
+            return dp[idx][buy][cap];
+        }
+        
+        // jb hum stock lete hai to purse price km hoga isliye minus hai
+        if(buy) {       // buy
+            return dp[idx][buy][cap] = max(-prices[idx] + find(idx + 1, n, 0, cap, prices, dp),         // buy stock here & cap remains same
+                                            0 + find(idx + 1, n, 1, cap, prices, dp)        // not buyed stock here & cap remains same
+                                            );
+        } else {        // sell
+            // jb hum stock profit me sell karte hai to purse price badhega 
+            return dp[idx][buy][cap] = max(prices[idx] + find(idx + 1, n, 1,  cap - 1, prices, dp),         // sold stock here & cap reduced as sold
+                                            0 + find(idx + 1, n, 0, cap, prices, dp)                    // not sold stock here so cap remains same
+                                            );
+        }
+
+    }
+    int maxProfit(int k, vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(k + 1, -1)));      
+
+        return find(0, n, 1, k, prices, dp);        
+    }
+};

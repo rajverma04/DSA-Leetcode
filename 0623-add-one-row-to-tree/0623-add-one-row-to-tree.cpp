397a869ef6b3c217fb3e4dcaf1ae45d6fe4676e4
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2026-02-14 00:51:22 +0530

    Time: 0 ms (100%), Space: 26.3 MB (8.5%) - LeetHub

---
+++ 0623-add-one-row-to-tree/0623-add-one-row-to-tree.cpp
@@ -0,0 +1,61 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* addOneRow(TreeNode* root, int val, int depth) {
+        if(depth == 1) {
+            TreeNode* newRoot = new TreeNode(val);
+            newRoot->left = root;
+            return newRoot;
+        }
+
+        queue<pair<TreeNode*, int>> q;        // {root, level}
+        q.push({root, 1});
+
+        while(!q.empty()) {
+            int level = q.front().second;
+            int sz = q.size();
+
+            if(level == depth - 1) {
+                for(int i = 0; i < sz; i++) {
+                    TreeNode* node = q.front().first;
+                    q.pop();
+
+                    TreeNode* leftChild = node->left;
+                    TreeNode* rightChild = node->right;
+
+                    TreeNode* newLeft = new TreeNode(val);
+                    TreeNode* newRight = new TreeNode(val);
+
+                    node->left = newLeft;
+                    node->right = newRight;
+
+                    newLeft->left = leftChild;
+                    newRight->right = rightChild;
+                }
+                break;
+            }
+            for(int i = 0; i < sz; i++) {
+                TreeNode *temp = q.front().first;
+                q.pop();
+                if(temp->left) {
+                    q.push({temp->left, level + 1});
+                }
+                if(temp->right) {
+                    q.push({temp->right, level + 1});
+                }
+            }
+        }
+
+        return root;
+    }
+};

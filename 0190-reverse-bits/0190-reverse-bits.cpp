27aa3a441a4ef1efd8fa2e5612edbb6e77e55977
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2026-02-16 10:23:25 +0530

    Time: 0 ms (100%), Space: 8.2 MB (46.07%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int reverseBits(int n) {
+        int ans = 0;
+        for(int i = 0; i < 32; i++) {
+            ans = ans << 1;         // make space
+            ans = ans | (n & 1);    // add last bit
+            n = n >> 1;             // remove last bit
+        }
+        return ans;
+    }
+};

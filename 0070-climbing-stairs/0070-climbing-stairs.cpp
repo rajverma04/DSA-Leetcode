434802c317b21da6afbc4ac70a2eed12c7413ce4
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2026-02-18 10:51:55 +0530

    Time: 0 ms (100%), Space: 8.7 MB (17.85%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int waysPossible(int n, vector<int> &dp) {
-        if(n == 0) {
+        if(n == 0) {        
             return 1;
         }
         if(n < 0) {
@@ -11,10 +11,10 @@ public:
             return dp[n];
         }
 
-        int left = waysPossible(n - 1, dp);     // for 1 step taking at a time
-        int right = waysPossible(n - 2, dp);    // for 2 step taking at a time
+        int oneStep = waysPossible(n - 1, dp);     // for 1 step taking at a time
+        int twoStep = waysPossible(n - 2, dp);    // for 2 step taking at a time
 
-        return dp[n] = left + right;
+        return dp[n] = oneStep + twoStep;
     }
     int climbStairs(int n) {
         vector<int> dp(n + 1, -1);

ab741182d9193f53a43cc72a6e276cdd0d5a1f08
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2026-02-18 11:23:16 +0530

    Time: 63 ms (11.49%), Space: 84.3 MB (5.36%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int find(int i, int j, string s1, string s2, vector<vector<int>> &dp) {
-        if(i < 0) {
-            return j + 1;
+        if(i < 0) {         // s1 empty string ho gaya hai
+            return j + 1;       // aur s2 me j + 1 char hai
         }
-        if(j < 0) {
-            return i + 1;
+        if(j < 0) {         // s2 empty string ho gaya
+            return i + 1;       // aur s2 me i + 1 char hai
         }
         if(dp[i][j] != -1) {
             return dp[i][j];
@@ -15,8 +15,11 @@ public:
             return dp[i][j] = find(i - 1, j - 1, s1, s2, dp);
         }
 
-        return dp[i][j] = 1 + min(find(i - 1, j, s1, s2, dp), 
-                                min(find(i, j - 1, s1, s2, dp), find(i - 1, j - 1, s1, s2, dp))
+        return dp[i][j] = 1 + min(                                  // 1 -> cost of performing one step
+                                find(i - 1, j, s1, s2, dp),         // delete
+                                min(
+                                    find(i, j - 1, s1, s2, dp),     // insert 
+                                    find(i - 1, j - 1, s1, s2, dp)) // replace
                                 );
     }
     int minDistance(string s1, string s2) {

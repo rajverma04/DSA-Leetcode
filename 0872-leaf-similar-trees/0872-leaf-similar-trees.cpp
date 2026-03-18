054b7f512c3315fc161e1c9b5fbe44693126f37d
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2026-03-18 15:23:01 +0530

    Time: 0 ms (100%), Space: 15.4 MB (50.32%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
@@ -25,15 +25,7 @@ public:
         vector<int> s1, s2;
         preorder(root1, s1);
         preorder(root2, s2);
-        if(s1.size() != s2.size()) {
-            return false;
-        }
-        for(int i = 0; i < s1.size(); i++) {
-            if(s1[i] != s2[i]) {
-                return false;
-            }
-        }
-
-        return true;
+        
+        return s1 == s2;
     }
 };

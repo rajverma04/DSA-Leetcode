806a7a4594792c11e8bec6eba179b8a4fa58d794
Raj Verma <147264174+rajverma04@users.noreply.github.com>
2026-04-09 00:26:16 +0530

    Time: 20 ms (61.99%), Space: 43.5 MB (72.4%) - LeetHub

--- 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.cpp
+++ 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.cpp
@@ -9,6 +9,23 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+/**
+    Approach:
+
+    1. We serialize each subtree into a string using preorder traversal 
+       (root, left, right).
+
+    2. Store each serialized string in a hash map (unordered_map) 
+       to keep track of how many times a subtree has appeared.
+
+    3. If a serialized string is already present in the map with frequency = 1,
+       it means this subtree is seen for the second time → it's a duplicate,
+       so we add its root to the answer.
+
+    4. We use a delimiter (",") between values and "N" for NULL nodes 
+       to ensure unique structure representation and avoid ambiguity.
+*/
 class Solution {
 public:
     string solve(TreeNode* root, vector<TreeNode*> &ans, unordered_map<string, int> &mp) {
